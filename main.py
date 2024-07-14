98a04c3f2ac847d51d827bc78493971c5131a19e
whqtker <chosh2001@naver.com>
2024-07-14 23:52:20 +0900

    insert 구현

--- main.py
+++ main.py
@@ -17,32 +17,19 @@ db_url = f'postgresql://{username}:{password}@{host}:{port}/{database}'
 # 데이터베이스 엔진 생성
 engine = create_engine(db_url, echo=False)
 
-metadata_obj = MetaData(bind=engine)
-
+metadata_obj = MetaData()
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 db = SessionLocal()
 
-def create_table_and_insert():
-    Base = declarative_base()
-
-    class User(Base):
-        __tablename__ = 'temp_table'
-
-        id = Column(Integer, primary_key=True)
-        name = Column(String)
-        email = Column(String)
-
-    new_users = [
-        {'name': 'John Doe', 'email': 'john.doe@example.com'},
-        {'name': 'Jane Smith', 'email': 'jane.smith@example.com'},
-        {'name': 'Bob Johnson', 'email': 'bob.johnson@example.com'}
-    ]
-
-    with engine.begin() as conn:
-        conn.execute(User.__table__.insert(), new_users)
+def insert(table_name, data):
+    table = Table(table_name, metadata_obj, autoload_with=engine)
+    ins = table.insert().values(data)
+    db.execute(ins)
+    db.commit()
+    db.close()
 
 def main():
-    create_table_and_insert()
+    insert('student', {'id': 96541, 'name': 'Alice', 'dept_name': 'Comp. Sci.', 'tot_cred': 5})
 
 if __name__ == '__main__':
     main()
